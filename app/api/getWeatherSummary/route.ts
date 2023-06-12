76bc71e9bf6c2d1d32f25f7d2ac5c61be90ea66d
Daniel Pichardo <danielpichardo716@gmail.com>
2023-06-12 11:02:30 -0400

    url update

--- app/api/getWeatherSummary/route.ts
+++ app/api/getWeatherSummary/route.ts
@@ -6,20 +6,24 @@ export async function POST(request: Request) {
   const { weatherData } = await request.json();
 
   const response = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo',
+    model: "gpt-3.5-turbo",
     temperature: 0.8,
     n: 1,
     stream: false,
     messages: [
       {
-        role: 'system',
-        content: "Pretend you're a weather news presenter presenting live on television. State the city you are providing a summary for, then give a summary of todays weather only."
-      }, {
-        role: 'user',
-        content: `Hi there, can I get a summary of todays weather, use the following information to get the weather data: ${JSON.stringify(weatherData)} and make sure to convert the temperatures from Celsius to Farenheit.. `
-      }
-    ]
-  })
+        role: "system",
+        content:
+          "Pretend you're a weather news presenter presenting live on television. State the city you are providing a summary for, then give a summary of todays weather only.",
+      },
+      {
+        role: "user",
+        content: `Hi there, can I get a summary of todays weather, use the following information to get the weather data: ${JSON.stringify(
+          weatherData
+        )} and make sure to convert the temperatures from Celsius to Farenheit.. `,
+      },
+    ],
+  });
 
   const { data } = response;
 
